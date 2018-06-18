532b1adedb0bae3d28b84d0ed6f6d51412e50b70
Peter-Chou <zhou_00009@126.com>
2018-06-18 16:24:33 +0800

    watch just .py file change to start pytest

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/bash
-# 第一个参数指定pytest 要跑的
-# 第二个及其后都是when-changed 监控的文件
+#!/usr/bin/sh
 
-# when-changed -v -1 "${@:1}" -c pytest -v -s $1
-when-changed -1 -r * -c pytest -v -s $1
+# when-changed -1 -r * -c pytest -v -s $1
+
+# /cygdrive/c/cygwin/bin/find * -name "*.py" | xargs -0 -I {} when-changed -1 -r {} -c pytest -v -s $1
+
+pathargs=$(/cygdrive/c/cygwin/bin/find * -name "*.py" | sed 's@/@\\@g') # solve windows path
+when-changed -1 -v $pathargs -c pytest -v -s $1
