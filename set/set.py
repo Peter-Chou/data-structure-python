9a6b159c75bcef36ba557d65e676191ad8f4f8f6
Peter-Chou <zhou_00009@126.com>
2018-06-20 09:47:55 +0800

    add set submodel

---
+++ set/set.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from ..hashtable import HashTable
+
+
+class SetADT(HashTable):
+
+    def add(self, key):
+        return super(SetADT, self).add(key, True)
+
+    def __add__(self, other_set):
+        new_set = SetADT()
+        for element_a in self:
+            if element_a in other_set():
+                new_set.add(element_a)
+        return new_set
+
+    def __sub__(self, other_set):
+        new_set = SetADT()
+        for element_a in self:
+            if element_a not in other_set:
+                new_set.add(element_a)
+        return new_set
+
+    def __or__(self, other_set):
+        new_set = SetADT()
+        for element_a in self:
+            new_set.add(element_a)
+        for element_b in other_set:
+            new_set.add(element_b)
+        return new_set
+
+    def __xor__(self, other_set):
+        return (self | other_set) - (self & other_set)
